dd68ea6ffb6089683d13e2e90835dc8864c283c3
Erwan Pichon <erwanpichon@MacBook-Pro-de-Erwan.local>
2019-05-01 16:54:24 +0200

    new files

---
+++ views/index/home.php
@@ -0,0 +1,82 @@
+<section class="home home-one parallax parallax-one">
+    <div class="container">
+        <h1 class="title">Titre</h1>
+        <p>Eius populus ab incunabulis primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur
+        </p>
+    </div>
+</section>
+<section class="b-beige home home-two static static-one">
+    <div class="container">
+        <h2 class="title c-white">Titre</h2>
+        <p class="c-white">Alios autem dicere aiunt multo etiam inhumanius (quem locum breviter paulo ante
+            perstrinxi) praesidii
+            adiumentique causa, non benevolentiae neque caritatis, amicitias esse expetendas; itaque, ut quisque
+            minimum firmitatis haberet minimumque virium, ita amicitias appetere maxime; ex eo fieri ut mulierculae
+            magis amicitiarum praesidia quaerant quam viri et inopes quam opulenti et calamitosi quam ii qui
+            putentur beati.</p>
+    </div>
+</section>
+<section class="home home-three parallax parallax-two">
+    <div class="container">
+        <h2 class="title">Titre</h2>
+        <p>Eius populus ab incunabulis primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur Eius populus ab incunabulis
+            primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur Eius populus ab incunabulis
+            primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur
+            Eius populus ab incunabulis primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur
+        </p>
+    </div>
+</section>
+<section class="b-beige home home-four static static-two">
+    <h2 class="title c-white">Titre</h2>
+    <div class="container">
+        <p class="c-white">Eius populus ab incunabulis primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur Eius populus ab incunabulis
+            primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur Eius populus ab incunabulis
+            primis ad usque
+            pueritiae tempus extremum, quod annis
+            circumcluditur pueritiae tempus extremum, quod annis
+            circumcluditurpueritiae tempus extremum, quod annis
+            circumcluditurpueritiae tempus extremum, quod annis
+            circumcluditurpueritiae tempus extremum, quod annis
+            circumcluditurpueritiae tempus extremum, quod annis
+        </p>
+        <img src="" alt="alt">
+    </div>
+</section>
+<section class="home home-five static static-three b-white">
+    <h2 class="title">Titre</h2>
+    <form action="post" class="container">
+        <div class="container">
+            <div class="input">
+                <input type="text" placeholder="Nom">
+                <input type="text" placeholder="Prénom">
+                <input type="text" placeholder="E-mail">
+            </div>
+            <p>Eius populus ab incunabulis primis ad usque
+                pueritiae tempus extremum, quod annis
+                circumcluditur Eius populus ab incunabulis
+                primis ad usque
+                pueritiae tempus extremum, quod annis
+                circumcluditur Eius populus ab incunabulis
+                primis ad usque
+                pueritiae tempus extremum, quod annis
+                circumcluditur
+            </p>
+        </div>
+        <textarea name="commentaire" placeholder="Commentaire"></textarea>
+        <input class="btn" type="submit">
+    </form>
+</section>
