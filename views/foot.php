3ea36e255aa294d9f19a6f55829d1bdb0d55037c
Erwan Pichon <erwanpichon@MacBook-Pro-de-Erwan.local>
2019-05-01 15:43:28 +0200

    foot and head html

--- views/foot.php
+++ views/foot.php
@@ -1,4 +1,45 @@
-
-  
+<footer class="b-beige">
+    <div class="container">
+        <ul>
+            <li class="title-ul">aide & informations</li>
+            <li class="li">Assistance</li>
+            <li class="li">Suivi commande</li>
+            <li class="li">Livraisons & Retours</li>
+            <li class="li">-15% pour les étudiants</li>
+        </ul>
+        <ul>
+            <li class="title-ul">à propos de m&1pousse</li>
+            <li class="li"><span>à</span> propos de Mille et une Pousse</li>
+            <li class="li">Mille et une Pousse recrute</li>
+            <li class="li">Responsabilités des entreprises</li>
+            <li class="li">Investisseurs</li>
+        </ul>
+        <ul>
+            <li class="title-ul">encore plus</li>
+            <li class="li">Chèque cadeaux</li>
+            <li class="li">Summer Days</li>
+            <li class="li">Mobile & appli</li>
+            <li class="li">Marketplace</li>
+            <li class="li">Soldes</li>
+        </ul>
+        <ul>
+            <li class="title-ul">sélectionner le pays</li>
+            <li class="li li-language">
+                <p>Vous êtes en</p>
+                <div class="flag"></div>
+                <hr>
+                <span class="change">changer</span>
+            </li>
+        </ul>
+    </div>
+    <div class="icons">
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-snapchat-ghost"></i>
+    </div>
+</footer>
+<script src="main.js"></script>
 </body>
+
 </html>
--- views/head.php
+++ views/head.php
@@ -1,10 +1,64 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Mille et Une Pousses</title>
-  <link rel="icon" href="favicon.ico">
-  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="stylesheet" href="./../src/styles/style.css">
+  <link rel="stylesheet" href="./../src/styles/section.css">
+  <title>MilleEtUnePousse</title>
 </head>
+
 <body>
+  <nav class="b-beige">
+    <ul>
+      <li>
+        <a class="c-white rout" value="blog" href="">nos boxs
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="profile" href="">produits
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="produits" href="">blog
+          <span class="underline"></span>
+        </a>
+      </li>
+    </ul>
+    <a class="logo b-white" href="">
+      <img class="c-white" src="./../img/Logo_pousse.png" alt="logo">
+    </a>
+    <ul>
+      <li>
+        <a class="c-white rout" value="blog" href="">nos boxs
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="profile" href="">produits
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="boxs" href="">mon compte
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="produits" href="">blog
+          <span class="underline"></span>
+        </a>
+      </li>
+      <li>
+        <a class="c-white rout" value="panier" href="">panier
+          <span class="underline"></span>
+        </a>
+      </li>
+    </ul>
+  </nav>
