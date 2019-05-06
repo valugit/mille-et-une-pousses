b7ca8078807b91225a03bae96dad97dbbd350c70
Erwan Pichon <erwanpichon@MacBook-Pro-de-Erwan.local>
2019-05-06 23:40:03 +0200

    modif footer

--- views/foot.php
+++ views/foot.php
@@ -2,25 +2,25 @@
     <div class="container">
         <ul>
             <li class="title-ul">aide & informations</li>
-            <li class="li">Assistance</li>
-            <li class="li">Suivi commande</li>
-            <li class="li">Livraisons & Retours</li>
-            <li class="li">-15% pour les étudiants</li>
+            <li class="li"><a>Assistance</a></li>
+            <li class="li"><a>Suivi commande</a></li>
+            <li class="li"><a>Livraisons & Retours</a></li>
+            <li class="li"><a>-15% pour les étudiants</a></li>
         </ul>
         <ul>
             <li class="title-ul">à propos de m&1pousse</li>
-            <li class="li"><span>à</span> propos de Mille et une Pousse</li>
-            <li class="li">Mille et une Pousse recrute</li>
-            <li class="li">Responsabilités des entreprises</li>
-            <li class="li">Investisseurs</li>
+            <li class="li"><a><span>à</span> propos de Mille et une Pousse</a></li>
+            <li class="li"><a>Mille et une Pousse recrute</a></li>
+            <li class="li"><a>Responsabilités des entreprises</a></li>
+            <li class="li"><a>Investisseurs</a></li>
         </ul>
         <ul>
             <li class="title-ul">encore plus</li>
-            <li class="li">Chèque cadeaux</li>
-            <li class="li">Summer Days</li>
-            <li class="li">Mobile & appli</li>
-            <li class="li">Marketplace</li>
-            <li class="li">Soldes</li>
+            <li class="li"><a>Chèque cadeaux</a></li>
+            <li class="li"><a>Summer Days</a></li>
+            <li class="li"><a>Mobile & appli</a></li>
+            <li class="li"><a>Marketplace</a></li>
+            <li class="li"><a>Soldes</a></li>
         </ul>
         <ul>
             <li class="title-ul">sélectionner le pays</li>
@@ -33,10 +33,19 @@
         </ul>
     </div>
     <div class="icons">
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-facebook-square"></i>
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-snapchat-ghost"></i>
+        <a href="">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="">
+            <i class="fab fa-facebook-square"></i>
+        </a>
+        <a href="">
+            <i class="fab fa-instagram"></i>
+
+        </a>
+        <a href="">
+            <i class="fab fa-snapchat-ghost"></i>
+        </a>
     </div>
 </footer>
 <script src="main.js"></script>
