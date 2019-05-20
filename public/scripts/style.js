454bbc28c3d58a2dd7cf7164ffcf037eca7835af
Erwan Pichon <erwanpichon@MacBook-Pro-de-Erwan.local>
2019-05-20 15:15:51 +0200

    logo fixed

--- public/scripts/style.js
+++ public/scripts/style.js
@@ -3,15 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener('scroll', () => {
 
         let navbar = document.querySelector('#navbar'),
-            logo = document.querySelector('#logo'),
             scroll = document.documentElement
 
         if (scroll.scrollTop >= 50) {
             navbar.classList.add('navbar-scroll')
-            logo.classList.add('logoExit')
         } else {
             navbar.classList.remove('navbar-scroll')
-            logo.classList.remove('logoExit')
         }
     })
 
