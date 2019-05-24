732fc9eddebf34045e295e0a95ea3c2781b15634
ErwanPichon <erwan.pichon@supinternet.fr>
2019-05-25 00:47:07 +0200

    new folder with leafs

--- src/views/connexion.php
+++ src/views/connexion.php
@@ -10,15 +10,16 @@
     <img src="" alt="">
 
     <section class="b-white">
-        <span class="b-lightPink"></span>
+        <span class="circle b-lightPink"></span>
         <h1 class="c-deepBlue">se connecter</h1>
         <form action="post">
             <input class="text b-white c-deepBlue" type="text" placeholder="adresse mail">
             <input class="text b-white c-deepBlue" type="text" placeholder="mot de passe">
-            <p>Alios autem dicere aiunt multo etiam inhumanius (quem locum breviter paulo ante perstrinxi) praesidii
-                adiumentique causa, non benevolentiae neque caritatis, amicitias esse expetendas; itaque, ut quisque
-                minimum</p>
-            <input class="btn btn-pinkOrange b-pinkOrange c-white" type="submit" value="valider">
+            <p class="c-deepBlue">Rentrez votre adresse mail ainsi que votre mot de passe afin de vous connecter</p>
+            <input class="btn btn-pinkOrange b-pinkOrange c-white" type="submit" value="valider">            
+            <a class="c-lightPink" href="">mot de passe oublié ?
+                <span class="underline b-lightPink"></span>
+            </a>
         </form>
     </section>
 </main>
